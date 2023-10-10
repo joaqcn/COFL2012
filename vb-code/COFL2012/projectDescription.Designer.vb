8cffcd9290bd871d778f676bdfaab8ac418e3279
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-10-10 10:27:16 -0400

    Created project description view

---
+++ vb-code/COFL2012/projectDescription.Designer.vb
@@ -0,0 +1,37 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class projectDescription
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' projectDescription
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1351, 838)
+        MaximizeBox = False
+        Name = "projectDescription"
+        Text = "projectDescription"
+        ResumeLayout(False)
+    End Sub
+End Class
---
+++ vb-code/COFL2012/projectDescription.vb
@@ -0,0 +1,3 @@
+﻿Public Class projectDescription
+
+End Class
