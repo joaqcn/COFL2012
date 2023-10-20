eeec0f8989995cfce282c383447e05e6cfde8d11
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-10-20 11:24:36 -0400

    Added buttons to  4X4

--- vb-code/COFL2012/4X4.Designer.vb
+++ vb-code/COFL2012/4X4.Designer.vb
@@ -50,6 +50,8 @@ Partial Class _4X4
         PictureBox8 = New PictureBox()
         Label19 = New Label()
         Label8 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
         CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
@@ -303,11 +305,37 @@ Partial Class _4X4
         Label8.TabIndex = 21
         Label8.Text = resources.GetString("Label8.Text")
         ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.AppWorkspace
+        Button1.Font = New Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.ForeColor = SystemColors.ControlText
+        Button1.Location = New Point(1094, 749)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(105, 36)
+        Button1.TabIndex = 22
+        Button1.Text = "Previous"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.AppWorkspace
+        Button2.Font = New Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.ForeColor = SystemColors.ControlText
+        Button2.Location = New Point(1205, 749)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(105, 36)
+        Button2.TabIndex = 25
+        Button2.Text = "Run"
+        Button2.UseVisualStyleBackColor = False
+        ' 
         ' _4X4
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1351, 838)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
         Controls.Add(Label8)
         Controls.Add(Label16)
         Controls.Add(Label19)
@@ -373,4 +401,6 @@ Partial Class _4X4
     Friend WithEvents PictureBox8 As PictureBox
     Friend WithEvents Label19 As Label
     Friend WithEvents Label8 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
