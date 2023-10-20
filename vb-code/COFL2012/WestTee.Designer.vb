b5c69c71f090bd09b77ccaa0b7b898ea71b62977
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-10-20 12:40:30 -0400

    Finished WestTee interface

--- vb-code/COFL2012/WestTee.Designer.vb
+++ vb-code/COFL2012/WestTee.Designer.vb
@@ -22,9 +22,298 @@ Partial Class WestTee
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "WestTee"
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(WestTee))
+        Button2 = New Button()
+        Button1 = New Button()
+        Label8 = New Label()
+        PictureBox8 = New PictureBox()
+        TextBox12 = New TextBox()
+        TextBox11 = New TextBox()
+        Label18 = New Label()
+        Label17 = New Label()
+        Label4 = New Label()
+        PictureBox3 = New PictureBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label5 = New Label()
+        PictureBox2 = New PictureBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.AppWorkspace
+        Button2.Font = New Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.ForeColor = SystemColors.ControlText
+        Button2.Location = New Point(1207, 744)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(105, 36)
+        Button2.TabIndex = 111
+        Button2.Text = "Run"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.AppWorkspace
+        Button1.Font = New Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.ForeColor = SystemColors.ControlText
+        Button1.Location = New Point(1096, 744)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(105, 36)
+        Button1.TabIndex = 110
+        Button1.Text = "Previous"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.BorderStyle = BorderStyle.FixedSingle
+        Label8.Location = New Point(39, 650)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(236, 167)
+        Label8.TabIndex = 109
+        Label8.Text = resources.GetString("Label8.Text")
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.Image = My.Resources.Resources.blue_arrow_top
+        PictureBox8.Location = New Point(682, 671)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(150, 94)
+        PictureBox8.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox8.TabIndex = 106
+        PictureBox8.TabStop = False
+        ' 
+        ' TextBox12
+        ' 
+        TextBox12.Location = New Point(576, 725)
+        TextBox12.Name = "TextBox12"
+        TextBox12.Size = New Size(100, 23)
+        TextBox12.TabIndex = 104
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Location = New Point(576, 676)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(100, 23)
+        TextBox11.TabIndex = 102
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label18.Location = New Point(439, 723)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(132, 42)
+        Label18.TabIndex = 101
+        Label18.Text = "Approach Traffic" & vbCrLf & " (veh/hr)"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label17.Location = New Point(452, 674)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(104, 21)
+        Label17.TabIndex = 98
+        Label17.Text = "Speed (mph)"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(196, 462)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(122, 25)
+        Label4.TabIndex = 97
+        Label4.Text = "EASTBOUND"
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = My.Resources.Resources.blue_arrow_right
+        PictureBox3.Location = New Point(301, 429)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(104, 94)
+        PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox3.TabIndex = 96
+        PictureBox3.TabStop = False
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(301, 386)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 95
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(301, 337)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 94
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(163, 384)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(132, 42)
+        Label6.TabIndex = 93
+        Label6.Text = "Approach Traffic" & vbCrLf & " (veh/hr)"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(181, 337)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(104, 21)
+        Label7.TabIndex = 92
+        Label7.Text = "Speed (mph)"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(568, 107)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(140, 25)
+        Label5.TabIndex = 91
+        Label5.Text = "SOUTHBOUND"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.blue_arrow_down
+        PictureBox2.Location = New Point(569, 125)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(150, 94)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 90
+        PictureBox2.TabStop = False
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(861, 174)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 89
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(861, 125)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 88
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(725, 174)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(132, 42)
+        Label3.TabIndex = 87
+        Label3.Text = "Approach Traffic" & vbCrLf & " (veh/hr)"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(751, 123)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(104, 21)
+        Label2.TabIndex = 86
+        Label2.Text = "Speed (mph)"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Semibold", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(501, 21)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(360, 37)
+        Label1.TabIndex = 85
+        Label1.Text = "Intersection Data - West Tee"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.West_Tee
+        PictureBox1.Location = New Point(407, 225)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(531, 440)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 84
+        PictureBox1.TabStop = False
+        ' 
+        ' WestTee
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1351, 838)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label8)
+        Controls.Add(PictureBox8)
+        Controls.Add(TextBox12)
+        Controls.Add(TextBox11)
+        Controls.Add(Label18)
+        Controls.Add(Label17)
+        Controls.Add(Label4)
+        Controls.Add(PictureBox3)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox4)
+        Controls.Add(Label6)
+        Controls.Add(Label7)
+        Controls.Add(Label5)
+        Controls.Add(PictureBox2)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "WestTee"
+        Text = "WestTee"
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
