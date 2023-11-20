bb29c51d87d3dce1ac126390dbec78d51719ac28
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-11-20 12:04:38 -0500

    Added toolbar to 4x6

--- vb-code/COFL2012/4X6.vb
+++ vb-code/COFL2012/4X6.vb
@@ -18,93 +18,93 @@ Public Class _4X6
     ''*************************************************************************************************************************************
     ''************************************************Top Navigation Menus and Buttons*****************************************************
     ''*************************************************************************************************************************************
-    'Private Sub ToolStripButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton1.Click
-    '    ClearAll()
-    '    District.InitDistrict()
-    '    IntersectionType.InitIntType()
-    '    My.Forms.Title.Show()
-    '    Me.Visible = False
-    '    Title.InitTitle()
-    'End Sub
+    Private Sub ToolStripButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton1.Click
+        Welcome.ClearAll()
+        District.InitDistrict()
+        IntersectionType.InitIntType()
+        My.Forms.Title.Show()
+        Me.Visible = False
+        Title.InitTitle()
+    End Sub
 
-    'Private Sub ToolStripButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton2.Click
-    '    OpenFileDialog1.InitialDirectory = PrimaryDirectory & "\UsrFiles\"
-    '    OpenFileDialog1.Filter = "Input files (*in)|*.in|All files (*.*)|*.*"
-    '    OpenFileDialog1.FileName = ""
+    Private Sub ToolStripButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton2.Click
+        OpenFileDialog1.InitialDirectory = Welcome.PrimaryDirectory & "\UsrFiles\"
+        OpenFileDialog1.Filter = "Input files (*in)|*.in|All files (*.*)|*.*"
+        OpenFileDialog1.FileName = ""
 
-    '    If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
-    '        FDOTin = OpenFileDialog1.FileName
-    '        OpenFile()
-    '        My.Forms.Title.Show()
-    '        Me.Visible = False
-    '        Title.InitTitle()
-    '        District.InitDistrict()
-    '        IntersectionType.InitIntType()
-    '    End If
-    'End Sub
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Welcome.FDOTin = OpenFileDialog1.FileName
+            Welcome.OpenFile()
+            My.Forms.Title.Show()
+            Me.Visible = False
+            Title.InitTitle()
+            District.InitDistrict()
+            IntersectionType.InitIntType()
+        End If
+    End Sub
 
-    'Private Sub ToolStripButton3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton3.Click
-    '    'Writes user program run inputs to text file
-    '    SaveFileDialog1.InitialDirectory = PrimaryDirectory & "\UsrFiles\"
-    '    SaveFileDialog1.Filter = "Input files (*.in)|*.in|All files (*.*)|*.*"
-    '    SaveFileDialog1.FileName = ""
+    Private Sub ToolStripButton3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton3.Click
+        'Writes user program run inputs to text file
+        SaveFileDialog1.InitialDirectory = Welcome.PrimaryDirectory & "\UsrFiles\"
+        SaveFileDialog1.Filter = "Input files (*.in)|*.in|All files (*.*)|*.*"
+        SaveFileDialog1.FileName = ""
 
-    '    If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
-    '        FDOTin = SaveFileDialog1.FileName
-    '        SaveFile()
-    '    End If
-    'End Sub
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            Welcome.FDOTin = SaveFileDialog1.FileName
+            Welcome.SaveFile()
+        End If
+    End Sub
 
     'Private Sub AboutToolStripButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripButton.Click
     '    My.Forms.About.Show()
     'End Sub
 
 
-    'Private Sub ToolStripButton12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton12.Click
-    '    My.Forms.About.Show()
-    'End Sub
+    Private Sub ToolStripButton9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton9.Click
+        My.Forms.About.Show()
+    End Sub
 
-    'Private Sub ToolStripButton7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton7.Click
-    '    QuickVerify = True
-    '    Verify4X6Form()
-    '    QuickVerify = False
-    '    My.Forms.Title.Show()
-    '    Me.Visible = False
-    'End Sub
+    Private Sub ToolStripButton4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton4.Click
+        Welcome.QuickVerify = True
+        Verify4X6Form()
+        Welcome.QuickVerify = False
+        My.Forms.Title.Show()
+        Me.Visible = False
+    End Sub
 
-    'Private Sub ToolStripButton8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton8.Click
-    '    QuickVerify = True
-    '    Verify4X6Form()
-    '    QuickVerify = False
-    '    My.Forms.District.Show()
-    '    Me.Visible = False
-    'End Sub
+    Private Sub ToolStripButton5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton5.Click
+        Welcome.QuickVerify = True
+        Verify4X6Form()
+        Welcome.QuickVerify = False
+        My.Forms.District.Show()
+        Me.Visible = False
+    End Sub
 
 
-    'Private Sub ToolStripButton9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton9.Click
-    '    QuickVerify = True
-    '    Verify4X6Form()
-    '    QuickVerify = False
-    '    My.Forms.IntersectionType.Show()
-    '    Me.Visible = False
-    'End Sub
+    Private Sub ToolStripButton6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton6.Click
+        Welcome.QuickVerify = True
+        Verify4X6Form()
+        Welcome.QuickVerify = False
+        My.Forms.IntersectionType.Show()
+        Me.Visible = False
+    End Sub
 
-    'Private Sub ToolStripButton11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton11.Click
-    '    'Call the routine to check form C
-    '    Verify4X6Form()
+    Private Sub ToolStripButton8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton8.Click
+        'Call the routine to check form C
+        Verify4X6Form()
 
-    '    'Verifies that all input forms have been completed before making a run
-    '    If (InputsCorrect(1) = True And InputsCorrect(2) = True And InputsCorrect(3) = True And InputsCorrect(4) = True) Then
-    '        Build4X6()
-    '        MakeARun()
-    '        Me.Visible = False
-    '    Else
-    '        If InputsCorrect(4) = True Then
-    '            MsgBox("A run cannot be made until all input forms have been completed.", 0, "CO Florida 2012")
-    '        End If
-    '    End If
+        'Verifies that all input forms have been completed before making a run
+        If (Welcome.InputsCorrect(1) = True And Welcome.InputsCorrect(2) = True And Welcome.InputsCorrect(3) = True And Welcome.InputsCorrect(4) = True) Then
+            Welcome.Build4X6()
+            Welcome.MakeARun()
+            Me.Visible = False
+        Else
+            If Welcome.InputsCorrect(4) = True Then
+                MsgBox("A run cannot be made until all input forms have been completed.", 0, "CO Florida 2012")
+            End If
+        End If
 
-    'End Sub
+    End Sub
 
     '************************************************************************************************************************
     '***************************************Initializes 4X6 Form*************************************************************
