fab055b77cd25a7df13e73757fb4414f6cdae396
joaqcn <joaqcncn@gmail.com>
2023-11-28 12:00:56 -0500

    #6 Fixing missing toolbar for West Tee

--- vb-code/COFL2012/WestTee.vb
+++ vb-code/COFL2012/WestTee.vb
@@ -2,9 +2,105 @@
 Imports System.Text.Encodings
 
 Public Class WestTee
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    '*************************************************************************************************************************************
+    '******************************************************Form Load and Close Subroutines***********************************************************
+    '*************************************************************************************************************************************
+    Private Sub I08_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        InitWestTee()
+    End Sub
+
+    Private Sub Form_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Application.Exit()
+    End Sub
+
+
+
+    '*************************************************************************************************************************************
+    '************************************************Top Navigation Menus and Buttons*****************************************************
+    '*************************************************************************************************************************************
+    Private Sub NewToolStripButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NewToolStripButton.Click
+        Welcome.ClearAll()
+        District.InitDistrict()
+        IntersectionType.InitIntType()
+        My.Forms.Title.Show()
+        Me.Visible = False
+        Title.InitTitle()
+    End Sub
+
+    Private Sub ToolStripButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OpenToolStripButton.Click
+        OpenFileDialog1.InitialDirectory = Welcome.PrimaryDirectory & "\UsrFiles\"
+        OpenFileDialog1.Filter = "Input files (*in)|*.in|All files (*.*)|*.*"
+        OpenFileDialog1.FileName = ""
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Welcome.FDOTin = OpenFileDialog1.FileName
+            Welcome.OpenFile()
+            My.Forms.Title.Show()
+            Me.Visible = False
+            Title.InitTitle()
+            District.InitDistrict()
+            IntersectionType.InitIntType()
+        End If
+    End Sub
+
+    Private Sub ToolStripButton3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveToolStripButton.Click
+        'Writes user program run inputs to text file
+        SaveFileDialog1.InitialDirectory = Welcome.PrimaryDirectory & "\UsrFiles\"
+        SaveFileDialog1.Filter = "Input files (*.in)|*.in|All files (*.*)|*.*"
+        SaveFileDialog1.FileName = ""
+
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            Welcome.FDOTin = SaveFileDialog1.FileName
+            Welcome.SaveFile()
+        End If
+    End Sub
+
+    Private Sub AboutToolStripButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripButton.Click
+        My.Forms.About.Show()
+    End Sub
+
+    Private Sub ToolStripButton7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TitleToolStripButton.Click
+        Welcome.QuickVerify = True
+        VerifyWestTeeForm()
+        Welcome.QuickVerify = False
+        My.Forms.Title.Show()
+        Me.Visible = False
+    End Sub
+
+    Private Sub ToolStripButton8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DistrictToolStripButton.Click
+        Welcome.QuickVerify = True
+        VerifyWestTeeForm()
+        Welcome.QuickVerify = False
+        My.Forms.District.Show()
+        Me.Visible = False
+    End Sub
+
+    Private Sub ToolStripButton9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles IntersectionTypeToolStripButton.Click
+        Welcome.QuickVerify = True
+        VerifyWestTeeForm()
+        Welcome.QuickVerify = False
+        My.Forms.IntersectionType.Show()
+        Me.Visible = False
+    End Sub
+
+    Private Sub ToolStripButton11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RunToolStripButton.Click
+        'Call the routine to check form inputs
+        VerifyWestTeeForm()
+
+        'Verifies that all input forms have been completed before making a run
+        If (Welcome.InputsCorrect(1) = True And Welcome.InputsCorrect(2) = True And Welcome.InputsCorrect(3) = True And Welcome.InputsCorrect(4) = True) Then
+            Welcome.BuildWestTee()
+            Welcome.MakeARun()
+            Me.Visible = False
+        Else
+            If Welcome.InputsCorrect(4) = True Then
+                MsgBox("A run cannot be made until all input forms have been completed.", 0, "CO Florida 2012")
+            End If
+        End If
 
     End Sub
+
+
 
 
     '************************************************************************************************************************
